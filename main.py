fa470055babdd62c6727794289875c3159c3a814
VerYT1lt <animefilm23@yandex.ru>
2024-06-11 21:33:21 +0300

    Config dz2

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-import json
-
-
-data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-}
-
-
-json_data = json.dumps(data)
-
-with open('data.json', 'w') as file:
-    file.write(json_data)
-
-print("JSON файл успешно создан.")
+#1
+# import json
+#
+#
+# data = {
+#     "name": "John",
+#     "age": 30,
+#     "city": "New York"
+# }
+#
+#
+# json_data = json.dumps(data)
+#
+# with open('data.json', 'w') as file:
+#     file.write(json_data)
+#
+# print("JSON файл успешно создан.")
+#2
+# import csv
+# data = [    ["Country", "Region", "Sales Revenue", "Number of Orders", "Number of Customers"],
+#     ["USA", "East", 50000, 200, 150],
+#     ["USA", "West", 75000, 300, 200],
+#     ["Canada", "East", 30000, 150, 100],
+#     ["Canada", "West", 45000, 250, 150]
+# ]
+#
+# with open("data.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(data)
